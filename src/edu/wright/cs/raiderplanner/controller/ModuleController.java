7d96e6a1b73049d719990d496d2c3eba429bb5b4
natedunn2230 <dunn.113@wright.edu>
2019-03-20 18:46:12 -0400

    Added copywright info and added name to authors

--- src/edu/wright/cs/raiderplanner/controller/ModuleController.java
+++ src/edu/wright/cs/raiderplanner/controller/ModuleController.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2019 - Nathan Dunn
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package edu.wright.cs.raiderplanner.controller;
 
 import edu.wright.cs.raiderplanner.model.Module;
@@ -18,7 +36,7 @@ import java.util.ResourceBundle;
 
 
 /**
- * Created by natedunn2230.
+ * Created by Nathan Dunn.
  */
 public class ModuleController implements Initializable {
 	private Module module;
