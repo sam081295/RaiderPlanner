b60f183682e0bc229ff4cb7e4d4fdabfee44e092
Caylor Wright <wright.407@wright.edu>
2019-03-23 22:41:26 -0400

    Limit character input in textField's and textArea's

--- src/edu/wright/cs/raiderplanner/controller/RequirementController.java
+++ src/edu/wright/cs/raiderplanner/controller/RequirementController.java
@@ -42,6 +42,8 @@ import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter; 
+import javafx.scene.control.TextFormatter.Change;
 import javafx.scene.control.Tooltip;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseButton;
@@ -116,7 +118,23 @@ public class RequirementController implements Initializable {
 	/**
 	 * Handle changes to the input fields.
 	 */
+	
 	public void handleChange() {
+		// Limit number of char's typed textArea & textField
+		this.details.setTextFormatter(new TextFormatter<String>(change -> 
+			change.getControlNewText().length() <= 400 ? change : null));
+		
+		this.name.setTextFormatter(new TextFormatter<String>(change -> 
+			change.getControlNewText().length() <= 100 ? change : null));
+		
+		this.quantity.setTextFormatter(new TextFormatter<String>(change -> 
+			change.getControlNewText().length() <= 100 ? change : null));
+		
+		this.time.setTextFormatter(new TextFormatter<String>(change -> 
+			change.getControlNewText().length() <= 50 ? change : null));
+		
+		this.quantityName.setTextFormatter(new TextFormatter<String>(change -> 
+			change.getControlNewText().length() <= 100 ? change : null));
 		// Check the input fields:
 		if (!this.name.getText().trim().isEmpty()
 				&& !this.quantity.getText().trim().isEmpty()
