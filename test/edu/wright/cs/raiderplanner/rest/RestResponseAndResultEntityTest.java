1336957897c448da6a4c2a561d9eb8ec10a68229
git-ben18 <zook.6@wright.edu>
2019-04-17 19:35:29 -0400

    Cut debugging statements, comments.

--- test/edu/wright/cs/raiderplanner/rest/RestResponseAndResultEntityTest.java
+++ test/edu/wright/cs/raiderplanner/rest/RestResponseAndResultEntityTest.java
@@ -46,7 +46,6 @@ public class RestResponseAndResultEntityTest {
 
 	@Test
 	public void responseEntityIsReceived() throws IOException {
-		//URL url = new URL("https://ipvigilante.com/68.1.1.1");
 		URL url = new URL("https://data.usajobs.gov/api/search?Keyword=Software");
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
 		con.setRequestProperty("Authorization-Key", "J30THoB+Rjb2iHSuhXkuoGY4ZPSlzO1RVzRUrY/AlYQ=");
@@ -60,58 +59,33 @@ public class RestResponseAndResultEntityTest {
 		ObjectMapper objectMapper = new ObjectMapper();
 		objectMapper.configure(Feature.AUTO_CLOSE_SOURCE, true);
 		try {
-			//IpAddress ipAddress = objectMapper.readValue(build.toString(), IpAddress.class); //new  URL("https://ipvigilante.com/68.1.1.1")
 			JsonNode responseNode = new ObjectMapper().readTree(build.toString());
 			JsonNode responseResultNode = responseNode.path("SearchResult");
 			ResponseUsaJobs responseJobs = new ResponseUsaJobs();
 			responseJobs.setLanguageCode(responseNode.get("LanguageCode").textValue());
-			//System.out.println(responseResultNode.get("SearchResultItems"));
-			
-			//System.out.println(responseResultNode);
 			ArrayNode itemArray = (ArrayNode) responseResultNode.path("SearchResultItems");
 			JsonNode positionNode = responseResultNode.path("MatchedObjectID");
 			System.out.println(positionNode);
-			
 			Iterator<JsonNode> itemIterator = itemArray.elements();
 			int counter = 0;
-			
 			JsonNode position = itemArray.findValue("PositionTitle");
 			JsonNode positionLocation = itemArray.findValue("PositionLocation");
-			//JsonNode organization = itemArray.findValue("OrganizationName");
 			String organization = itemArray.findValue("OrganizationName").toString();
-			//JsonNode positionURI = itemArray.findValue("PositionURI");
-//			System.out.println(position);
-//			System.out.println(positionLocation.findValue("CityName"));
-//			System.out.println(organization);
-//			System.out.println(positionURI);
-//			System.out.println(itemArray.findValue("PositionTitle"));
-//			System.out.println(itemArray.findValue("PositionLocation"));
-//			System.out.println(itemArray.findValue("PositionURI"));
-			//System.out.println(itemArray.get(1));
 			ArrayList positionList = new ArrayList<>();
 			ArrayList locationList = new ArrayList<>();
 			ArrayList organizationList = new ArrayList<>();
-			ArrayList positionURI = new ArrayList<>();
+			ArrayList positionUri = new ArrayList<>();
 			while (itemIterator.hasNext() & counter < 3) {
 				JsonNode item = itemIterator.next();
 				positionList.add(item.findValue("PositionTitle").toString());
 				locationList.add(item.findValue("CityName").toString());
 				organizationList.add(item.findValue("OrganizationName").toString());
-				positionURI.add(item.findValue("PositionURI").toString());
+				positionUri.add(item.findValue("PositionURI").toString());
 				counter++;
 			}
-
 			System.out.println(positionList.get(0));
 			System.out.println(positionList.get(1));
 			System.out.println(positionList.get(2));
-			//System.out.println(itemList.get(0));
-			//System.out.println(itemList.get(1));
-
-
-			//ResponseUsaJobs response = objectMapper
-			//		.readValue(build.toString(),ResponseUsaJobs.class);
-			//assertEquals(response.getLanguageCode(), "success");
-			//assertEquals(response.getSearchResult(), "success");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
