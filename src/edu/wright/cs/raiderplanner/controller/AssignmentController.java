a765a7ebb2cda9f50a02a34a3eb168853fde52d4
natedunn2230 <dunn.113@wright.edu>
2019-04-20 16:48:07 -0400

    Fixing imports part 2

--- src/edu/wright/cs/raiderplanner/controller/AssignmentController.java
+++ src/edu/wright/cs/raiderplanner/controller/AssignmentController.java
@@ -21,20 +21,20 @@
 
 package edu.wright.cs.raiderplanner.controller;
 
-import edu.wright.cs.raiderplanner.model.Deadline;
 import edu.wright.cs.raiderplanner.model.Assignment;
-import edu.wright.cs.raiderplanner.model.Module;
+import edu.wright.cs.raiderplanner.model.Deadline;
 import edu.wright.cs.raiderplanner.model.Extension;
 import edu.wright.cs.raiderplanner.model.Event;
 import edu.wright.cs.raiderplanner.model.Coursework;
+import edu.wright.cs.raiderplanner.model.Module;
 import edu.wright.cs.raiderplanner.model.Person;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
