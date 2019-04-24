c92171079a5b8165b308a973934b3b898d6981cb
Robert Noonan <39569282+robNoonan@users.noreply.github.com>
2019-04-24 14:04:22 -0400

    Added encapsulation to the accordion function along with more questions and answers.

--- docs/assets/js/faq.js
+++ docs/assets/js/faq.js
@@ -5,4 +5,10 @@ $(document).ready(function(){
 		$(this).parent().siblings().children('.help-content').slideUp();
 		$(this).parent().siblings().removeClass('active');
 	});
+	$('.acc h2').click(function(){
+		$(this).next('.help-content').slideToggle();
+		$(this).parent().toggleClass('active');
+		$(this).parent().siblings().children('.help-content').slideUp();
+		$(this).parent().siblings().removeClass('active');
+	});
 });
