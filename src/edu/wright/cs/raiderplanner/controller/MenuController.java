a0f6111e4861e3fc4f3e932017279c8f56052498
TommyVaughn <54631562+TommyVaughn@users.noreply.github.com>
2019-10-07 22:06:54 -0400

    Update MenuController.java

--- src/edu/wright/cs/raiderplanner/controller/MenuController.java
+++ src/edu/wright/cs/raiderplanner/controller/MenuController.java
@@ -361,8 +361,7 @@ public class MenuController implements Initializable {
 				== 0) {
 			VBox dashPic = new VBox();
 			//dashPic.autosize();
-			ImageView dashImg = new ImageView(new Image
-							  ("/edu/wright/cs/raiderplanner/content/DashBoardHelp.png"));
+			ImageView dashImg = new ImageView(new Image("/edu/wright/cs/raiderplanner/content/DashBoardHelp.png"));
 			dashPic.getChildren().add(dashImg);
 			dashPic.setAlignment(Pos.CENTER);
 			dashPic.setMinWidth(150);
